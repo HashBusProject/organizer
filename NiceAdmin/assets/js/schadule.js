7acbee750d438d1011b96f8287fb2c8d35c80b58
saif <saifomar1952001@gmail.com>
2023-12-22 16:58:17 +0300

    holiday

--- NiceAdmin/assets/js/schadule.js
+++ NiceAdmin/assets/js/schadule.js
@@ -60,9 +60,71 @@ $(document).ready(function () {
     });
     $('#example').on('click', '.edit-button', function () {
         var data = $("#example").DataTable().row($(this).parents('tr')).data();
-        console.log("saif")  ;
+        var time = data.time;
+        var journeySelectEdit = document.getElementById("journeyNameEdit");
+        var bus_id_edit = document.getElementById("busIdEdit");
+
+        document.getElementById("timeInputEdit").value = time.substring(0, time.length - 3);
+        if(journeySelectEdit.length > 0 )
+            journeySelectEdit.innerHTML ='' ; 
+        if(bus_id_edit.length > 0)
+            bus_id_edit.innerHTML = ""; 
+        $.ajax({
+            url: "http://localhost:8080/Organizer/GetAllJourneys",
+            method: "GET",
+            contentType: 'application/json',
+            success: function (journeys) {
+                for (var i = 0; i < journeys.length; i++) {
+                    if(data.journey == journeys[i].id){
+                    var option = document.createElement("option");
+                    option.value = journeys[i].id;
+                    option.text = journeys[i].name;
+                    journeySelectEdit.appendChild(option);
+                }
+            }
+                
+
+                for (var i = 0; i < journeys.length; i++) {
+                    if(data.journey != journeys[i].id){
+                    var option = document.createElement("option");
+                    option.value = journeys[i].id;
+                    option.text = journeys[i].name;
+                    journeySelectEdit.appendChild(option);
+                }
+            }
+
+                $.ajax({
+                    url: "http://localhost:8080/Organizer/GetIdOfBuses",
+                    method: "GET",
+                    contentType: 'application/json',
+                    success: function (busIds) {
+                        for (var i = 0; i < busIds.length; i++) {
+                            if(data.bus == busIds[i].id){
+                            var option = document.createElement("option");
+                            option.value = busIds[i];
+                            option.text = busIds[i];
+                            bus_id_edit.appendChild(option);
+                            
+                        }
+                        for (var i = 0; i < busIds.length; i++) {
+                            if(data.bus != busIds[i].id){
+                            var option = document.createElement("option");
+                            option.value = busIds[i];
+                            option.text = busIds[i];
+                            bus_id_edit.appendChild(option);
+                            
+                        }
+                    }
+                    }
+                }
+                });
+
+                $('#editTripModal').modal('show');
+            }
+        });
     });
-    
+
+
     $('#example').on('click', '.delete-button', function () {
         var data = $("#example").DataTable().row($(this).parents('tr')).data();
         console.log(data) ;
@@ -82,8 +144,15 @@ $(document).ready(function () {
     
 });
 
+
+
 function showAddFeild() {
     var journeySelect = document.getElementById("journeyName") ; 
+    var bus_id = document.getElementById("busID");
+    if(journeySelect.length > 0 && bus_id.length > 0  ){
+        $('#addTrip').modal('show');
+        return;
+    }
     $.ajax({
         url : "http://localhost:8080/Organizer/GetAllJourneys" , 
         method : "GET" , 
@@ -95,8 +164,23 @@ function showAddFeild() {
                 option.text = journey[i].name;
                 journeySelect.appendChild(option);
         }
-        $('#addTrip').modal('show');
+
+            $.ajax({
+                url : "http://localhost:8080/Organizer/GetIdOfBuses" ,
+                method : "GET" , 
+                contentType: 'application/json',
+                success:function(busId){
+                    for(var i = 0 ; i < busId.length ; i++){
+                        var option = document.createElement("option") ; 
+                        option.value = busId[i];
+                        option.text = busId[i];
+                        bus_id.appendChild(option);
+                    }
+                }
+            });
+            $('#addTrip').modal('show');
         } , 
+
 
     });
 }
@@ -105,5 +189,51 @@ function addSchedule(){
     var journey = document.getElementById("journeyName").value ; 
     var bus = document.getElementById("busID").value ; 
     var time = document.getElementById("timeInput").value ; 
-    alert(journey + " " + bus + " " + time) ; 
+    time += ":00" ; 
+    schedule = { 
+        journey : journey , 
+        bus : bus , 
+        time : time 
+    }
+
+    $.ajax({
+        url : "http://localhost:8080/Organizer/AddSchedule" , 
+        method : "POST" , 
+        contentType: 'application/json',
+        data : JSON.stringify(schedule) ,
+        success:function(data) {
+            alert("Schedule added successfully!") ; 
+        } , 
+        error:function(error) {
+            alert("Error in add schedule") ; 
+        }
+    });
+
 }
+
+
+function editTrip() {
+    var schedule_id = document.getElementById("idTripEdit").value ; 
+    var bus = document.getElementById("busIdEdit").value ; 
+    var journey = document.getElementById("journeyNameEdit").value; 
+    var time = document.getElementById("timeInputEdit").value;
+    time += ":00" ;
+    var schedule = {
+        scheduleId : schedule_id , 
+        bus : bus , 
+        journey : journey  , 
+        time : time
+    }
+    $.ajax({
+        url :  "http://localhost:8080/Organizer/EditSchedule",
+        method : "PUT" , 
+        contentType: 'application/json',
+        data : JSON.stringify(schedule) ,
+        success:function(data) {
+            alert("Trip updated successfully!") ; 
+        } , 
+        error:function(data) {
+            alert("Error in update trip") ; 
+        }
+    });
+};
